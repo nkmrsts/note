98eba99d4473f21e6583514ce6f1b7de8224862b
nkmrsts <29856021+nkmrsts@users.noreply.github.com>
2019-06-21 08:55:26 +0900

    なんかいろいろやった

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,53 @@ import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField'
 import './App.css';
 
-
+interface Note {
+  text: string,
+  id: number
+}
 
 const App: React.FC = () => {
-  const [text,setText] = useState('')
+  const [text, setText] = useState('')
+  const [items, setItems] = useState<Note[]>([])
+  const [currentNote, setCurrentNote] = useState(1)
+  console.log(items)
 
-  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) =>{
-    setText(e.target.value)
+  const handleChange = (text: string) =>{
+    setText(text)
+    onUpDate(currentNote)
   }
+  const onUpDate = (id:number) => {
+    if(items.length === 0) {
+      setItems([...items, { text, id }])
+    }else {
+      const index = items.findIndex(item => item.id === id)
+      const _items = [...items]
+      _items[index] = {..._items[index], text}
+      setItems(_items)
+    }
+  }
+  const onAdd = () => {
+    setCurrentNote(items.length + 1)
+    setItems([...items, { text:'', id: items.length + 1 }])
+    setText('')
+  }
+
   return (
     <Fragment>
       <Container maxWidth="md">
-        <TextField value={text} onChange={handleChange} fullWidth multiline rows="10" variant="outlined" />
-        <p>{text}</p>
+        <p>今表示してるノート No.{currentNote}</p>
+        <button onClick={onAdd}>plus</button>
+        <TextField value={text} onChange={e=>handleChange(e.target.value)} fullWidth multiline rows="10" variant="outlined" />
+        <p>リスト</p>
+        <ul>
+          {items.map((item,i)=> (
+            <li key={i}>
+              <p>No.{item.id}</p>
+              <p>本文:{item.text}</p>
+              <p>日付</p>
+            </li>
+          ))}
+        </ul>
       </Container>
     </Fragment>
   );
