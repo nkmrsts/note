0986a885a67623523066d5342d5dc73b48b1ebb7
nkmrsts <29856021+nkmrsts@users.noreply.github.com>
2019-06-22 06:43:01 +0900

    delete機能をつける

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React, { Fragment,useState } from 'react'
-import Container from '@material-ui/core/Container';
-import TextField from '@material-ui/core/TextField'
-import Box from '@material-ui/core/Box'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemText from '@material-ui/core/ListItemText'
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
-import IconButton from '@material-ui/core/IconButton'
-import DeleteIcon from '@material-ui/icons/Delete';
-
+import {
+  Container,
+  TextField,
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
+  IconButton
+} from '@material-ui/core'
+import DeleteIcon from '@material-ui/icons/Delete'
 import './App.css';
 
 interface Note {
@@ -18,27 +19,36 @@ interface Note {
 
 const App: React.FC = () => {
   const [text, setText] = useState('')
-  const [items, setItems] = useState<Note[]>([])
   const [currentNote, setCurrentNote] = useState(1)
+  const [count, setcount] = useState(1)
+  const [items, setItems] = useState([{
+    text: '',
+    id: count
+  }])
 
   const handleChange = (text: string) =>{
     setText(text)
-    onUpDate(currentNote)
+    onUpDate(text,currentNote)
   }
-  const onUpDate = (id:number) => {
-    if(items.length === 0) {
-      setItems([{ text, id }])
-    }else {
-      const index = items.findIndex(item => item.id === id)
-      const _items = [...items]
-      _items[index] = {..._items[index], text}
-      setItems(_items)
-    }
+  const onUpDate = (text:string,id:number) => {
+    const index = items.findIndex(item => item.id === id)
+    const _items = [...items]
+    _items[index] = {..._items[index], text}
+    setItems(_items)
   }
   const onAdd = () => {
-    setCurrentNote(items.length + 1)
-    setItems([...items, { text:'', id: items.length + 1 }])
+    setCurrentNote(count + 1)
+    setItems([...items, { text:'', id: count + 1 }])
     setText('')
+    setcount(count + 1)
+  }
+  const onDelete = (id:number) => {
+    const _items = items.filter(item => item.id !== id)
+    setItems(_items)
+    if(currentNote === id) {
+      setCurrentNote(1)
+      setText('')
+    }
   }
   const onSelect = (id:number) => {
     if(currentNote === id) return
@@ -59,7 +69,7 @@ const App: React.FC = () => {
                 <ListItemText style={{ flex: '0 0 auto', marginRight: '10px' }} primary={`No.${item.id}`}/>
                 <ListItemText primary={item.text} secondary="日付" />
                 <ListItemSecondaryAction>
-                  <IconButton edge="end" aria-label="Delete">
+                  <IconButton edge="end" aria-label="Delete" onClick={()=>onDelete(item.id)}>
                     <DeleteIcon />
                   </IconButton>
                 </ListItemSecondaryAction>
