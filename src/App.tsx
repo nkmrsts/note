02cf07124f162c6cbf770314f20e3127000e3849
nkmrsts <29856021+nkmrsts@users.noreply.github.com>
2019-06-26 16:30:45 +0900

    ログイン

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
 import { CssBaseline } from '@material-ui/core'
 import { StylesProvider, ThemeProvider } from '@material-ui/styles'
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useState, useEffect } from 'react'
 import RouteHome from './components/RouteHome'
 import { createTheme } from './helpers/createTheme'
+import firebase from 'firebase/app'
 
 const App: FunctionComponent = () => {
+  const [user, setUser] = useState<object | null>(null)
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => {
+      console.log(user)
+      setUser(user)
+    })
+  }, [])
+
+  const login = () => {
+    const provider = new firebase.auth.GoogleAuthProvider()
+    firebase.auth().signInWithRedirect(provider)
+  }
+
+  const logout = () => {
+    firebase.auth().signOut()
+  }
+
   return (
     <StylesProvider>
       <ThemeProvider theme={createTheme()}>
         <CssBaseline />
+        <div>
+          {!user ? (
+            <button onClick={login}>Google Login</button>
+          ) : (
+            <button onClick={logout}>Google Logout</button>
+          )}
+        </div>
         <RouteHome />
       </ThemeProvider>
     </StylesProvider>
