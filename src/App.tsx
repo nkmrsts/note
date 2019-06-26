56f8445b366d0a66b7c86b655fb8e6f840b31543
reiwa <reiwa@outlook.com.vn>
2019-06-26 21:02:16 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,40 @@
 import { CssBaseline } from '@material-ui/core'
 import { StylesProvider, ThemeProvider } from '@material-ui/styles'
-import React, { FunctionComponent, useState, useEffect } from 'react'
+import firebase, { auth } from 'firebase/app'
+import React, { Fragment, FunctionComponent } from 'react'
 import RouteHome from './components/RouteHome'
+import { useAuthLoading } from './firebase/useAuthLoading'
+import { useAuthUser } from './firebase/useAuthUser'
 import { createTheme } from './helpers/createTheme'
-import firebase from 'firebase/app'
 
 const App: FunctionComponent = () => {
-  const [user, setUser] = useState<object | null>(null)
-  useEffect(() => {
-    firebase.auth().onAuthStateChanged(user => {
-      console.log(user)
-      setUser(user)
-    })
-  }, [])
+  const [authUser] = useAuthUser()
+
+  const [authLoading] = useAuthLoading()
 
   const login = () => {
     const provider = new firebase.auth.GoogleAuthProvider()
-    firebase.auth().signInWithRedirect(provider)
+    auth().signInWithRedirect(provider)
   }
 
   const logout = () => {
-    firebase.auth().signOut()
+    auth().signOut()
   }
 
   return (
     <StylesProvider>
       <ThemeProvider theme={createTheme()}>
         <CssBaseline />
-        <div>
-          {!user ? (
-            <button onClick={login}>Google Login</button>
-          ) : (
-            <button onClick={logout}>Google Logout</button>
-          )}
-        </div>
+        {authLoading && <button>{'loading...'}</button>}
+        {!authLoading && (
+          <Fragment>
+            {authUser === null ? (
+              <button onClick={login}>{'Google Login'}</button>
+            ) : (
+              <button onClick={logout}>{'Google Logout'}</button>
+            )}
+          </Fragment>
+        )}
         <RouteHome />
       </ThemeProvider>
     </StylesProvider>
