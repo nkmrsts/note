e6db9673f75f943c562794b74facb463f19d0e42
reiwa <reiwa@outlook.com.vn>
2019-06-30 21:34:58 +0700

    update: orderBy('createdAt', DESC)

--- src/shared/firestore/watchNotes.ts
+++ src/shared/firestore/watchNotes.ts
@@ -1,10 +1,13 @@
 import { firestore } from 'firebase/app'
 import { collectionData } from 'rxfire/firestore'
 import { NOTES } from './constants/collection'
+import { DESC } from './constants/order'
 import { Note } from './types/note'
 
 export const watchNotes = () => {
-  const notesRef = firestore().collection(NOTES)
+  const notesRef = firestore()
+    .collection(NOTES)
+    .orderBy('createdAt', DESC)
 
   return collectionData<Note>(notesRef)
 }
