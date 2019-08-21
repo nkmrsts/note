22c23a77a8ff58b60b0544a76a5377c06abe4fe2
reiwa <reiwa@outlook.com.vn>
2019-08-22 00:41:38 +0700

    refactor

--- src/shared/markdown/MarkdownPreview.tsx
+++ src/shared/markdown/MarkdownPreview.tsx
@@ -3,7 +3,6 @@ import { Value } from 'slate'
 import { Editor } from 'slate-react'
 import { renderBlock } from './components/renderBlock'
 import { renderMark } from './components/renderMark'
-import { onKeyDown } from './helpers/onKeyDown'
 import { plugins } from './helpers/plugins'
 
 type Props = { value: Value }
@@ -11,7 +10,6 @@ type Props = { value: Value }
 const MarkdownPreview: FunctionComponent<Props> = ({ value }) => {
   return (
     <Editor
-      onKeyDown={onKeyDown}
       plugins={plugins}
       readOnly
       renderBlock={renderBlock}
