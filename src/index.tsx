40dad7d6a79670e4c426a11fcdeea816e21f35cf
reiwa <reiwa@outlook.com.vn>
2019-10-09 23:26:09 +0700

    update: serviceWorker

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,31 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './firebase/initializeApp'
 import './index.css'
-import { unregister } from './serviceWorker'
+import { register } from './serviceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-unregister()
+register({
+  onUpdate(registration) {
+    if (!registration.waiting) return
+
+    const listener = (
+      event: Event & {
+        target: Partial<ServiceWorker> & EventTarget | null
+      }
+    ) => {
+      if (!event.target || event.target.state !== 'activated') return
+      window.location.reload()
+    }
+
+    registration.waiting.addEventListener('statechange', listener)
+
+    registration.waiting.postMessage({ type: 'SKIP_WAITING' })
+  }
+})
 
 if (module.hot) {
   module.hot.accept()
